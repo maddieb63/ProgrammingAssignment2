3ce14f1918df3734d83f2fb4de5bfda5551c7340
maddieb63 <maddiebaird1@gmail.com>
2026-03-24 16:31:04 -0700

    Included code from ArrayStack.h because it originally did not show up when downloaded, go this from a friend who it showed up for.

--- ArrayStack.h
+++ ArrayStack.h
@@ -5,8 +5,35 @@
 #ifndef PROGRAMMINGASSIGNMENT2_ARRAYSTACK_H
 #define PROGRAMMINGASSIGNMENT2_ARRAYSTACK_H
 
+#include<vector>
+#include <stdexcept>
+
+template<typename T>
 
 class ArrayStack {
+private:
+    std::vector<T> data;
+
+public:
+    void push(const T& item) {
+
+    }
+
+    void pop() {
+
+    }
+
+    T top() const {
+        return T();
+    }
+
+    bool empty() const {
+        return true;
+    }
+
+    int size() const {
+        return 0;
+    }
 };
 
 
