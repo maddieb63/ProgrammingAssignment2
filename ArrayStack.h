0d28b0d62fc9a0505d5ce5f11bfabdb7eff943ef
maddieb63 <maddiebaird1@gmail.com>
2026-03-29 13:54:59 -0700

    Finishing simpler ArraySTack methods and going to test pop and push, but not convinced they are perfect yet.

--- ArrayStack.h
+++ ArrayStack.h
@@ -13,35 +13,54 @@ template<typename T>
 class ArrayStack {
 private:
     std::vector<T> data;
+    int topIndex; // top index
 
 public:
+    ArrayStack() : topIndex(-1){}
     void push(const T& item) {
-        if // stack is not full (!isFull)
+        // stack is not full (!isFull)
         // increment top index number and make index[] equal to item
         // size limit?
+        topIndex++;
+        if (topIndex < data.size()) {
+
+            data.push_back(item);
+
+        }
+
 
     }
 
     void pop() {
-        if // stack is not empty (!isEmpty)
+        // stack is not empty (!isEmpty)
         // first in - first out
         // decrement top
         // size should automatically get smaller if top moves?
+        if (!empty()) {
+            data.pop_back();
+            topIndex--;
+        } else {
+            throw std::out_of_range("Stack is empty");
+        }
     }
 
     T top() const {
-        if // if not empty returns top data
-        return T();
+        if (!empty()) {
+            return data[topIndex];
+        } else {
+            throw std::out_of_range("Stack is empty");
+        }
+            // if not empty returns top data
     }
 
     bool empty() const {
         // only full if the top value is -1
-        return true;
+        return topIndex == -1;
     }
 
     int size() const {
         // return number of indexes + 1 for index 0
-        return 0;
+        return topIndex + 1;
     }
 };
 
